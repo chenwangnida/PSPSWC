b47ab5721ebec3cf1fa0ff530f81aab78a9bcb5d
chenwang <chenwangnida@gmail.com>
2017-03-14 15:50:25 +1300

    update

--- run_pso_grid_2009.sh
+++ run_pso_grid_2009.sh
@@ -5,5 +5,5 @@ need sgegrid
 NUM_RUNS=40
 
 for i in {1..5}; do
-  qsub -t 1-$NUM_RUNS:1 graph_pso.sh ~/workspace/swsc2009/Set0${i}MetaData 2009-semantic-pso${i};
+  qsub -t 1-$NUM_RUNS:1 graph_pso.sh ~/workspace/swsc2009/Set0${i}MetaData 2009-semantic-QoS-warepso${i};
 done
--- src/ec/pso/GraphPSO.java
+++ src/ec/pso/GraphPSO.java
@@ -692,8 +692,8 @@ public class GraphPSO {
 		t = normaliseTime(t);
 		c = normaliseCost(c);
 
-		individual.fitness = ((W1 * mt) + (W2 * dst) + (W3 * a) + (W4 * r) + (W5 * t) + (W6 * c));
-//		individual.fitness = ((0.25 * a) + (0.25 * r) + (0.25 * t) + (0.25 * c));
+//		individual.fitness = ((W1 * mt) + (W2 * dst) + (W3 * a) + (W4 * r) + (W5 * t) + (W6 * c));
+		individual.fitness = ((0.25 * a) + (0.25 * r) + (0.25 * t) + (0.25 * c));
 
 
 		return individual.fitness;
@@ -760,7 +760,7 @@ public class GraphPSO {
 						bestAvailSoFar.get(i), bestReliaSoFar.get(i), bestTimeSoFar.get(i), bestCostSoFar.get(i),
 						bestMatchTypeSoFar.get(i), bestSemanticDistanceSoFar.get(i)));
 			}
-			writer.append(finalGraph);
+//			writer.append(finalGraph);
 			writer.append("\n");
 			writer.close();
 
